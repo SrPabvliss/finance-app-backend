1d77f5ef21a75b928f2c372e56d99842d251ccc8
SrPabvliss <pablomartinvillacres@gmail.com>
2024-12-31 00:14:20 -0500

    feat: transactions module implemented

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,14 @@ import createApp from "./core/infrastructure/lib/create-app";
 import configureOpenAPI from "./core/infrastructure/lib/configure-open-api";
 import users from "@/users/infrastructure/controllers/user.controller";
 import paymentMethods from "@/payment-methods/infrastructure/controllers/payment-method.controller";
+import transactions from "@/transactions/infrastructure/controllers/transaction.controller";
 import DatabaseConnection from "@/db";
 
 const app = createApp();
 
 configureOpenAPI(app);
 
-const routes = [index, users, paymentMethods] as const;
+const routes = [index, users, paymentMethods, transactions] as const;
 
 app.get("/debug/db-status", (c) => {
 	const db = DatabaseConnection.getInstance();
